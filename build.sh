aa0ade5d6fe8347a9aa2a2c21b88db8d4f9aecb4
OctMon <octmon@qq.com>
2022-07-05 14:57:29 +0800

    Change 安卓、苹果Beta分开打包

--- build.sh
+++ build.sh
@@ -1,33 +1,59 @@
 pwd
 
-echo "输入项目名称"
+echo "---------------------------------"
+myArray=()
+
+index=0
+for entry in /Users/limeng/Developer/*
+do
+  myArray+=($entry)
+
+  echo "$index: $entry"
+  let "index++"
+done
+echo "---------------------------------"
+pwd
+
+echo
+echo "输入项目编号 0 - `expr ${#myArray[@]} - 1`"
 read answer
 
-project="/Users/limeng/Developer/$answer"
+project=${myArray[answer]}
 
 echo $project
+echo
 
-echo '输入 1 到 3 之间的数字:'
-echo '1: Beta'
-echo '2: Appstore'
-echo '3: Android'
+echo '输入 1 到 5 之间的数字:'
+echo '1: Beta-iOS'
+echo '2: Beta-android'
+echo '3: Beta-all'
+echo '4: Appstore'
+echo '5: Android'
 
 echo '你输入的数字为:'
 read aNum
 case $aNum in
 1)
   echo '你选择了 1'
-  sh build_beta.sh $project
+  sh build_beta.sh $project "ios"
   ;;
 2)
   echo '你选择了 2'
-  sh build_ipa.sh $project
+  sh build_beta.sh $project "android"
   ;;
 3)
   echo '你选择了 3'
+  sh build_beta.sh $project "ios" "android"
+  ;;
+4)
+  echo '你选择了 4'
+  sh build_ipa.sh $project
+  ;;
+5)
+  echo '你选择了 5'
   sh build_apk.sh $project
   ;;
 *)
-  echo '你没有输入 1 到 3 之间的数字'
+  echo '你没有输入 1 到 5 之间的数字'
   ;;
 esac
--- build_beta.sh
+++ build_beta.sh
@@ -1,31 +1,66 @@
 cd $1
 
+iosFlag=false
+androidFlag=false
+
+if [[ $2 = "ios" || $3 = "ios" ]]
+then
+  iosFlag=true
+  echo "☑︎ iOS"
+else
+  echo "☒︎ iOS"
+fi
+
+if [[ $2 = "android" || $3 = "android" ]]
+then
+  androidFlag=true
+  echo "☑︎ android"
+else
+  echo "☒︎ android"
+fi
+
 flutter clean
 
 flutter packages get
 
-flutter build ipa --export-method ad-hoc --dart-define=app-debug-flag=true --obfuscate --split-debug-info=symbols
+if [ $iosFlag == true ]; then
+  flutter build ipa --export-method ad-hoc --dart-define=app-debug-flag=true --obfuscate --split-debug-info=symbols
+fi
 
 name=$(cat pubspec.yaml | grep "name: " | awk '{print $2}' | head -n 1)
 
-build_name="build/ios/ipa/$name"
+build_ios_name="build/ios/ipa/$name"
+build_android_name="build/app/outputs/apk/release/$name"
 oss_path="oss://octmon/release/$name/"
 
 # open build/ios/ipa
 
-flutter build apk --dart-define=app-channel=beta --dart-define=app-debug-flag=true --obfuscate --split-debug-info=symbols
-
-mv build/app/outputs/apk/release/app-release.apk $build_name.apk
+if [ $androidFlag == true ]; then
+  flutter build apk --dart-define=app-channel=beta --dart-define=app-debug-flag=true --obfuscate --split-debug-info=symbols
+  
+  mv build/app/outputs/apk/release/app-release.apk $build_android_name.apk
+fi
 
 # open build/app/outputs/apk/release
 
 # ----- upload -----
 
-~/ossutil/ossutil64 cp $build_name.ipa $oss_path -f
-
-~/ossutil/ossutil64 cp $build_name.apk $oss_path -f
-
 version=$(cat pubspec.yaml | grep "version:" | awk '{print $2}')
-echo "{\"version\": \"$version\"}" > $build_name.version
 
-~/ossutil/ossutil64 cp $build_name.version $oss_path -f
+if [ $iosFlag == true ]; then
+  ~/ossutil/ossutil64 cp $build_ios_name.ipa $oss_path -f
+
+  ipa_version="${build_ios_name}_ipa".version
+  echo "{\"version\": \"$version\"}" > $ipa_version
+
+  ~/ossutil/ossutil64 cp $ipa_version $oss_path -f
+fi
+
+if [ $androidFlag == true ]; then
+  ~/ossutil/ossutil64 cp $build_android_name.apk $oss_path -f
+
+  apk_version="${build_android_name}_apk".version
+  echo "{\"version\": \"$version\"}" > $apk_version
+
+  ~/ossutil/ossutil64 cp $apk_version $oss_path -f
+fi
