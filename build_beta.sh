e07ed076055e216e7a34a286c23c8cf0eadadd4f
OctMon <octmon@qq.com>
2024-11-11 11:31:29 +0800

    chore: check whether ossutil is installed

--- build_beta.sh
+++ build_beta.sh
@@ -10,6 +10,32 @@ ossPath=$(cat pubspec.yaml | grep "ossPath: " | awk '{print $2}')
 iosFlag=false
 androidFlag=false
 
+
+if [[ -n "${pgyer_api_key}" ]]
+then
+  echo "pgyer_api_key存在"
+else
+  echo "pgyer_api_key不存在"
+fi
+
+ossUtilInstall=0
+if [[ -n "${accessKeySecret}" ]]
+then
+  echo "accessKeySecret存在"
+  if command -v ossutil >/dev/null 2>&1; then
+    ossUtilInstall=1
+    echo "ossutil已安装"
+    else
+    echo "请输入密码，安装ossutil"
+  #   默认安装到/usr/local/bin目录下。
+    sudo -v ; curl https://gosspublic.alicdn.com/ossutil/install.sh | sudo bash
+    echo "ossutil安装成功"
+    ossUtilInstall=1
+  fi
+else
+  echo "accessKeySecret不存在"
+fi
+
 if [[ $2 == "release" || $2 = "ios" || $3 = "ios" ]]
 then
   iosFlag=true
@@ -62,7 +88,6 @@ if [ $androidFlag == true ]; then
   fi
   echo "---------------------------------"
   echo
-  
   mv build/app/outputs/apk/release/app-release.apk $build_android_name.apk
 fi
 
@@ -70,67 +95,60 @@ fi
 
 # ----- upload -----
 
-ossUtilInstall=0
-if command -v ~/ossutil/ossutil64 >/dev/null 2>&1;then
-   ossUtilInstall=1
-fi
-
-echo $ossUtilInstall
-
 version=$(cat pubspec.yaml | grep "version:" | awk '{print $2}')
 
 if [ $iosFlag == true ]; then
 
   if [[ -n "${pgyer_api_key}" ]]
-      then
-          #上传到pgyer
-          echo "正在上传ipa到蒲公英..."
-          echo
-          file_ipa="$build_ios_name.ipa"
-#          curl -F "file=@${file_ipa}" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=脚本自动上传" https://www.pgyer.com/apiv2/app/upload
-          curl -F "file=@${file_ipa}" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=脚本自动上传" https://www.xcxwo.com/apiv2/app/upload
-          echo
-          say "iOS上传蒲公英成功"
-          echo
+  then
+    #上传到pgyer
+    echo "正在上传ipa到蒲公英..."
+    echo
+    file_ipa="$build_ios_name.ipa"
+    curl -F "file=@${file_ipa}" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=脚本自动上传" https://www.pgyer.com/apiv2/app/upload
+#    curl -F "file=@${file_ipa}" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=脚本自动上传" https://www.xcxwo.com/apiv2/app/upload
+    echo
+    say "iOS上传蒲公英成功"
+    echo
   fi
 
   if [ $ossUtilInstall -eq 1 ]; then
-      echo "正在上传ipa到OSS..."
-      echo
-      ~/ossutil/ossutil64 -e $endpoint -i $accessKeyID -k $accessKeySecret cp $build_ios_name.ipa $ossPath -f
+    echo "正在上传ipa到OSS..."
+    echo
+    ossutil -e $endpoint -i $accessKeyID -k $accessKeySecret cp $build_ios_name.ipa $ossPath -f
 
-      ipa_version="${build_ios_name}_ipa".version
-      echo "{\"version\": \"$version\"}" > $ipa_version
+    ipa_version="${build_ios_name}_ipa".version
+    echo "{\"version\": \"$version\"}" > $ipa_version
 
-      ~/ossutil/ossutil64 -e $endpoint -i $accessKeyID -k $accessKeySecret cp $ipa_version $ossPath -f
+    ossutil -e $endpoint -i $accessKeyID -k $accessKeySecret cp $ipa_version $ossPath -f
   fi
 
 fi
 
 if [ $androidFlag == true ]; then
 
-if [[ -n "${pgyer_api_key}" ]]
-      then
-          #上传到pgyer
-          echo "正在上传apk到蒲公英..."
-          echo
-          file_apk="$build_android_name.apk"
-#          curl -F "file=@${file_apk}" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=脚本自动上传" https://www.pgyer.com/apiv2/app/upload
-          curl -F "file=@${file_apk}" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=脚本自动上传" https://www.xcxwo.com/apiv2/app/upload
-          echo
-          say "android上传蒲公英成功"
-          echo
+  if [[ -n "${pgyer_api_key}" ]]
+  then
+    #上传到pgyer
+    echo "正在上传apk到蒲公英..."
+    echo
+    file_apk="$build_android_name.apk"
+  curl -F "file=@${file_apk}" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=脚本自动上传" https://www.pgyer.com/apiv2/app/upload
+#    curl -F "file=@${file_apk}" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=脚本自动上传" https://www.xcxwo.com/apiv2/app/upload
+    echo
+    say "android上传蒲公英成功"
+    echo
   fi
 
   if [ $ossUtilInstall -eq 1 ]; then
-      echo "正在上传apk到OSS..."
-      echo
-      ~/ossutil/ossutil64 -e $endpoint -i $accessKeyID -k $accessKeySecret cp $build_android_name.apk $ossPath -f
+    echo "正在上传apk到OSS..."
+    echo
+    ossutil -e $endpoint -i $accessKeyID -k $accessKeySecret cp $build_android_name.apk $ossPath -f
 
-      apk_version="${build_android_name}_apk".version
-      echo "{\"version\": \"$version\"}" > $apk_version
+    apk_version="${build_android_name}_apk".version
+    echo "{\"version\": \"$version\"}" > $apk_version
 
-      ~/ossutil/ossutil64 -e $endpoint -i $accessKeyID -k $accessKeySecret cp $apk_version $ossPath -f
+    ossutil -e $endpoint -i $accessKeyID -k $accessKeySecret cp $apk_version $ossPath -f
   fi
 
 fi
