deff77b51a878c76f9bdb790554899cce719e13e
OctMon <octmon@qq.com>
2024-11-12 22:08:01 +0800

    chore: plist generate

--- build_beta.sh
+++ build_beta.sh
@@ -121,8 +121,47 @@ if [ $iosFlag == true ]; then
     echo "{\"version\": \"$version\"}" > $ipa_version
 
     ossutil -e $oss_endpoint -i $oss_access_key_id -k $oss_access_key_secret cp $ipa_version $oss_upload_path -f
-  fi
 
+    # plist文件创建
+    distribution_summary_plist=build/ios/ipa/DistributionSummary.plist
+    ipa_version="1.0.0"
+    ipa_build="0"
+    ipa_package=""
+    if [ -e $distribution_summary_plist ]; then
+        echo "$distribution_summary_plist 文件存在"
+        ipa_version=$(/usr/libexec/PlistBuddy -c "Print :$name.ipa:0:versionNumber" $distribution_summary_plist)
+        ipa_build=$(/usr/libexec/PlistBuddy -c "Print :$name.ipa:0:buildNumber" $distribution_summary_plist)
+        ipa_package=$(/usr/libexec/PlistBuddy -c "Print :$name.ipa:0:entitlements:application-identifier" $distribution_summary_plist)
+        ipa_package="${ipa_package#*.}"
+        echo $ipa_version
+        echo $ipa_build
+        echo $ipa_package
+    else
+        echo "$distribution_summary_plist 文件不存在"
+    fi
+
+    info_plist=$build_ios_name.plist
+    if [ -e $info_plist ]; then
+        echo "$info_plist 文件存在"
+        rm -f $info_plist
+    else
+        echo "$info_plist 文件不存在"
+    fi
+
+    /usr/libexec/PlistBuddy -c "Add :items array" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0 dict" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:assets array" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:assets:0 dict" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:assets:0:url string '$oss_file_path$name.ipa'" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:assets:0:kind string 'software-package'" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:metadata dict" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:metadata:title string '$name'" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:metadata:kind string 'software'" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:metadata:bundle-version string '$ipa_version'" $info_plist
+    /usr/libexec/PlistBuddy -c "Add :items:0:metadata:bundle-identifier string '$ipa_package'" $info_plist
+
+    ossutil -e $oss_endpoint -i $oss_access_key_id -k $oss_access_key_secret cp $info_plist $oss_upload_path -f
+  fi
 fi
 
 if [ $androidFlag == true ]; then
@@ -176,7 +215,7 @@ echo "iOS扫码下载: "
 qrencode -m 2 -t UTF8 "itms-services://?action=download-manifest&url=$oss_file_path$name.plist"
 echo
 echo "Android扫码下载: "
-qrencode -m 2 -l Q -t UTF8 "$oss_file_path$name.apk"
+qrencode -m 4 -l M -t UTF8 "$oss_file_path$name.apk"
 echo
 echo
 echo
