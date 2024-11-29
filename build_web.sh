2a8cf978ccf9f456aac413faf1936f7f4c62dac0
OctMon <octmon@qq.com>
2024-11-29 10:40:27 +0800

    chore: build web rename project name

--- build_web.sh
+++ build_web.sh
@@ -1,5 +1,7 @@
 cd $1
 
+name=$(cat pubspec.yaml | grep "name: " | awk '{print $2}' | head -n 1)
+
 flutter clean
 
 flutter packages get
@@ -9,7 +11,7 @@ flutter build web
 cd build
 # 定义目录路径
 DIR_PATH="web"          # 需要压缩的目录
-OUTPUT_FILE="web.zip"   # 压缩后的文件名
+OUTPUT_FILE="$name.zip"   # 压缩后的文件名
 
 # 检查目录是否存在
 if [ ! -d "$DIR_PATH" ]; then
@@ -17,8 +19,12 @@ if [ ! -d "$DIR_PATH" ]; then
   exit 1
 fi
 
+ln -s $DIR_PATH $name
+
 # 压缩目录
-zip -r $OUTPUT_FILE $DIR_PATH
+zip -r $OUTPUT_FILE $name
+
+rm $name
 
 # 提示压缩完成
 echo "压缩完成：$OUTPUT_FILE"
